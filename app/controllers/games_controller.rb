f8f234a84d60c7cafc00acba78a5cb8b12400f3b
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-07 19:44:56 -0400

    it's working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,28 +3,27 @@ require "open-uri"
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
-
   end
 
   def score
-    @letters = params[:letters]
-    @english_word = english_word(@word)
+    @letters = params[:letters].split
+    @word = params[:word].upcase
     @included = included?(@word, @letters)
-    @word
+    @english_word = english_word?(@word)
   end
 
   private
 
   def included?(word, letters)
-    @word.chars.all? do |letter|
-      @word.count(letter) <= @letters.count(letter)
+    word.chars.all? do |letter|
+      word.count(letter) <= letters.count(letter)
     end
   end
 
-  def english_word(word)
-    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
     json_string = URI.open(url).read
     result = JSON.parse(json_string)
-    return result['found']
+    result['found']
   end
 end
